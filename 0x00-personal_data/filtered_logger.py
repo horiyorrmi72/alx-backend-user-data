1583a8be8f8b2fedead6422cb788ba76b2da678a
Yusuf Ridwan <horiyorrmi72@gmail.com>
2023-08-27 07:40:22 +0000

    update class and format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,3 +19,30 @@ def filter_datum(
         message = re.sub(i + "=.*?" + separator,
                          i + "=" + redaction + separator, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """returns get logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format method """
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super(RedactingFormatter,
+                                  self).format(record), self.SEPARATOR)
