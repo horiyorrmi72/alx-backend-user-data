91477cf9258fc5553c6c457208e0ac9a19592780
yusuf <horiyorrmi72@gmail.com>
2025-04-24 22:00:48 +0100

    feat(all files):add all needed files

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+""" Authentication module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Template class for all authentication systems"""
+
+    def require_auth(self, path: str, excluded_paths:  List[str]) -> bool:
+        """ Checks if the path requires authentication
+        Args:
+            path (str): the path to check
+            excluded_paths (list[str]): list of excluded paths
+        Returns:
+            bool: True if the path requires authentication, False otherwise
+            But for now it will onl return False
+        """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            else:
+                if not excluded_path.endswith('/'):
+                    path == excluded_path
+                if path == excluded_path:
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Returns the authorization header from the request
+        Args:
+            request: the request object
+        Returns:
+            str: the authorization header
+        """
+        if request is None or not request.headers:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Return None"""
+        return None
