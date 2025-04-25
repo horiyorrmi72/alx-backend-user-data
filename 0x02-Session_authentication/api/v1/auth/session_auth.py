b6912ccd45c33f99489460adc29d4e42f73fb5b0
yusuf <horiyorrmi72@gmail.com>
2025-04-25 11:17:44 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -61,7 +61,7 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         if not session_id:
             return False
-        user_id = self.user_id_for_session_id(request)
+        user_id = self.user_id_for_session_id(session_id)
         if not user_id:
             return False
 
