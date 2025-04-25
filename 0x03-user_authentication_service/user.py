6b4d964ab7b7a02ea9b0330e849e1a8774f3d11c
yusuf <horiyorrmi72@gmail.com>
2025-04-25 17:22:41 +0100

    fix(code styling):fix pycodestyle issue

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,16 +3,18 @@
 User model for a user authentication setup.
 """
 
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True) 
+    session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
