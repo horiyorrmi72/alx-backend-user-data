e330d5aad86271e9b14b88dd4a0a614e11411982
yusuf <horiyorrmi72@gmail.com>
2025-04-25 09:46:50 +0100

    feat(excluded lists):add '/api/v1/auth_session/login/' to the excluded lists

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,10 +53,12 @@ def before_request() -> None:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if not auth.authorization_header(request):
+    if not auth.authorization_header(request) or auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)
     if not auth.current_user(request):
         abort(403)
