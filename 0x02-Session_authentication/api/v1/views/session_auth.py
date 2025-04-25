f323ad261f59358360be3178ae90d86721e4e108
yusuf <horiyorrmi72@gmail.com>
2025-04-25 11:05:37 +0100

    feat(logout):add logout handler

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -22,7 +22,7 @@ def session_login():
         return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({"email":email})
+    users = User.search({"email":   email})
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
@@ -31,5 +31,13 @@ def session_login():
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     session_name = os.getenv("SESSION_NAME")
-    response.set_cookie(session_name,session_id)
+    response.set_cookie(session_name, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """Logs user out of session by deleting the session"""
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
