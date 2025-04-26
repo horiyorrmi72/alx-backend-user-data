0d34c7852959f85bd25ab0bc54dd9a94cfa121aa
yusuf <horiyorrmi72@gmail.com>
2025-04-26 07:57:44 +0100

    feat(register user):addd register user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,10 +3,34 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """Hash a password using bcrypt and return the hashed password"""
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+    hash_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hash_password.decode('utf-8')
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with the provided email and password"""
+        if not email or not password:
+            raise ValueError("Email and password are required")
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password=password)
+            return self._db.add_user(email=email,
+                                     hashed_password=hashed_password)
