2657818a79edb79bd155105d24ebef77e03c1ec2
yusuf <horiyorrmi72@gmail.com>
2025-04-26 12:50:11 +0100

    feat(update password):add update user pass route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,22 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": token}), 200
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """Update user password using reset token."""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if reset_token is None or new_password is None:
+        abort(403)
+
+    try:
+        AUTH.update_password(reset_token=reset_token, password=new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
