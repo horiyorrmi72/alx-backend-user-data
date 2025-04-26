416b77a4b61493dc4617610503a7e2b589af984a
yusuf <horiyorrmi72@gmail.com>
2025-04-26 10:47:42 +0100

    feat(user from session id):get user from session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,3 +65,14 @@ class Auth:
                 return user_new_session_id
         except NoResultFound:
             return
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """Gets user from session id and return user instance or
+        None if no session id or user found
+        """
+        if not session_id:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        if not user:
+            return None
+        return user
