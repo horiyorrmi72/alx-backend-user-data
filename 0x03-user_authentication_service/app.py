cc22f9157eebf893bdd0bed4f0187e661d7df1e7
yusuf <horiyorrmi72@gmail.com>
2025-04-26 11:58:05 +0100

    feat(user profile):add profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,17 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    user_session_id = request.cookies.get("session_id")
+    if user_session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id=user_session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
