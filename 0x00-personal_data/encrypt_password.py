847c66a398cf888ee7c9da44ed025255266b88f9
yusuf <horiyorrmi72@gmail.com>
2025-05-01 08:10:13 +0100

    feat(verify password):add function to verify password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,12 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """This hash user password and return salted"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    This function compare the provided plain password
+    with the password stored in DB and returns true if match
+    and false otherwise
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
